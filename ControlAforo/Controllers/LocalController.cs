555f263c566fbfb9d24c2e8c3f57bdc425a1cfa8
Ivan <ivandev98@gmail.com>
2020-11-19 18:23:39 +0100

    Primer comment

---
+++ ControlAforo/Controllers/LocalController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControlAforo.Controllers
+{
+    public class LocalController : Controller
+    {
+        // GET: Local
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GestionLocales()
+        {
+            return View();
+        }
+
+        // GET: Local/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Local/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Local/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Local/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Local/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Local/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Local/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
