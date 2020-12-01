2017721624ad5c77e0af2aae4ff78c5ea9da009e
douglas malave <doumalave@gmail.com>
2020-12-01 16:42:04 +0100

    Cambios de Ivan y 271 archivos

--- ControlAforo/Controllers/LocalController.cs
+++ ControlAforo/Controllers/LocalController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using ControlAforo.Models;
@@ -12,6 +14,7 @@ namespace ControlAforo.Controllers
 
         //Obtener el contexto de la BD.
         PACbdEntities1 db = new PACbdEntities1();
+        
 
         // GET: Local
         public ActionResult Index()
@@ -26,10 +29,7 @@ namespace ControlAforo.Controllers
         public ActionResult Details(int id)
         {
 
-            //List<Local> detalleLocal = db.Local.Where(l => l.id == id).ToList();
-            Local detalleLocal = db.Local.Find(id);
-
-
+           Local detalleLocal = db.Local.Find(id);
 
             return View(detalleLocal);
         }
@@ -48,15 +48,17 @@ namespace ControlAforo.Controllers
         {
             try
             {
-                // TODO: Add insert logic here
-
+                //Instanciamos el objeto Empresa mediante los datos obtenidos en el formulario.
                 Empresa e = new Empresa();
                 e.nombre = collection["empN"];
                 e.CIF = collection["cif"];
 
+                //Añadimos la empresa a la base de datos.
                 db.Empresa.Add(e);
+                //Guardamos la base de datos.
                 db.SaveChanges();
 
+                //Instanciamos el objeto Local mediante los datos obtenidos en el formulario.
                 Local l = new Local();
                 l.usuario = collection["user"];
                 l.contraseña = collection["password"];
@@ -65,7 +67,9 @@ namespace ControlAforo.Controllers
                 l.aforoMax = Int32.Parse(collection["afM"]);
                 l.id_empresa = Int32.Parse(e.id.ToString());
                 
+                //Añadimos el local a la base de datos.
                 db.Local.Add(l);
+                //Guardamos la base de datos.
                 db.SaveChanges();
 
                 return RedirectToAction("Index");
@@ -80,24 +84,64 @@ namespace ControlAforo.Controllers
         // Nos enviara a la página de edición del local.
         public ActionResult Edit(int id)
         {
-
+            //Instanciamos un objeto de tipo Empresa.
             Empresa em = new Empresa();
+            //Obtenemos la id de la empresa del local seleccionado.
             int id_empresa = db.Local.Find(id).id_empresa;
+            //Creamos le objeto empresa con los datos obtenidos de la BD.
             em = db.Empresa.Find(id_empresa);
 
-            Local lo = new Local();
-            lo = db.Local.Find(id);
+            //Almacenamos los atributos de la empresa en los ViewBags para poder usarlos en las vistas.
+            ViewBag.IdEmpresa = em.id.ToString();
+            ViewBag.NombreEmpresa = em.nombre;
+            ViewBag.CIF = em.CIF;
 
-            return View("~/Views/EditarLocal/Editar.cshtml");
+            //Instanciamos un objeto de tipo Local.
+            Local lo = new Local();
+            //Obtenemos los datos del local seleccionado mediante su id.
+            lo = db.Local.Find(id);
+            
+            //Almacenamos los atributos del local en los ViewBags para poder usarlo en las vistas.
+            ViewBag.IdLocal = lo.id.ToString();
+            ViewBag.Usuario = lo.usuario;
+            ViewBag.Contraseña = lo.contraseña;
+            ViewBag.Direccion = lo.direccion;
+            ViewBag.Telefono = lo.telefono.ToString();
+            ViewBag.AforoM = lo.aforoMax.ToString();
+            ViewBag.IdEmpresa = lo.id_empresa.ToString();
+
+            
+            return View("~/Views/Local/Edit.cshtml");
         }
 
         // POST: Local/Edit/5
+        //Obtenemos los datos del formulario y hacemos el update.
         [HttpPost]
         public ActionResult Edit(int id, FormCollection collection)
         {
             try
             {
-                // TODO: Add update logic here
+                
+                //Obtenemos la id de la empresa al enviar los datos del formulario.
+                int idEmpresa = Int32.Parse(collection["idEmpresa"]);
+
+                //Cambiamos los datos del nombre y CIF de la empresa por los datos obtenidos del formulario.
+                db.Empresa.Find(idEmpresa).nombre = collection["empN"];
+                db.Empresa.Find(idEmpresa).CIF = collection["cif"];
+
+                //Guardamos los cambios en la BD.
+                db.SaveChanges();
+
+                //Cambiamos los datos del usuario, contraseña, dirección, teléfono y aforo máximo del local.
+                db.Local.Find(id).usuario = collection["user"];
+                db.Local.Find(id).contraseña = collection["password"];
+                db.Local.Find(id).direccion = collection["dir"];
+                db.Local.Find(id).telefono = Int32.Parse(collection["tel"]);
+                db.Local.Find(id).aforoMax = Int32.Parse(collection["afM"]);
+                
+                //Guardamos los cambios en la BD.
+                db.SaveChanges();
+
 
                 return RedirectToAction("Index");
             }
@@ -110,10 +154,15 @@ namespace ControlAforo.Controllers
         // GET: Local/Delete/5
         public ActionResult Delete(int id)
         {
-            //Eliminar un local
+            //Instanciamos un local mediante los datos obtenidos de la base de datos usando la id del local.
             Local localEliminado = db.Local.Find(id);
+
+            //Eliminamos el local de la base de datos.
             db.Local.Remove(localEliminado);
+            
+            //Guardamos la base de datos.
             db.SaveChanges();
+            
             return RedirectToAction("Index");
         }
 
---
+++ ControlAforo/bin/app.publish/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ControlAforo.MvcApplication" Language="C#" %>
