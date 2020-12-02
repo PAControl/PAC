d3f20fb0a956e662441ec573d70d1d3bb336b9f0
douglas malave <doumalave@gmail.com>
2020-12-02 16:10:44 +0100

    haciendo el login

--- ControlAforo/Controllers/HomeController.cs
+++ ControlAforo/Controllers/HomeController.cs
@@ -26,5 +26,12 @@ namespace ControlAforo.Controllers
 
             return View();
         }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Please login";
+
+            return View();
+        }
     }
 }
